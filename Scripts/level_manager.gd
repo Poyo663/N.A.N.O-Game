11c425be7e74826f2b930b26cd040809f20cd0d3
Anna de Azevedo Maciel <annadeazevedo@annadeazevedo.annadeazevedo>
2024-05-09 15:26:26 -0300

    adicionei um evento pra mudar de nivel

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -2,22 +2,23 @@ extends Node2D
 
 signal on_level_pass()
 
+# para mudar de nivel use
+# emit_signal("on_level_pass")
+
 var currentLevelNumber = 0
 # não mecha nesse variável, essa variável deve ser preenchida apenas por script
 const levelPaths: Array[String] = [
 	"res://Levels/level_1.tscn",
 ]
-var currentLevel = preload(levelPaths[0])
+var currentLevel = preload(levelPaths[0]).instantiate()
 
 
 func _ready():
 	add_child(currentLevel)
 	connect("on_level_pass", _on_level_pass)
 
-	emit_signal("on_level_pass")
-
 func _on_level_pass():
 	currentLevelNumber += 1
 	remove_child(currentLevel)
-	currentLevel = load(levelPaths[currentLevelNumber])
+	currentLevel = load(levelPaths[currentLevelNumber]).instantiate()
 	add_child(currentLevel)
